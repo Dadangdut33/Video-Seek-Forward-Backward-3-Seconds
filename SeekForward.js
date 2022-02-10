e8ed612e4b69f79abb02fa842ec89cd9fc41992b
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-10 10:34:07 +0700

    added readme and comments

--- SeekForward.js
+++ SeekForward.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Video Seek Forward Backward 3 Seconds
-// @namespace    https://github.com/Dadangdut33
+// @namespace    https://github.com/Dadangdut33/Video-Seek-Forward-Backward-3-Seconds
 // @version      1.0
 // @description  Use ctrl + \ and ctrl + ] to seek video forward and backward 3 seconds. To seek forward use ctrl + \. To seek backward use ctrl + ]. You can modify the key and the seek time to your desire easily in the script.
 // @author       Dadangdut33
@@ -11,13 +11,17 @@
 (function () {
 	"use strict";
 
-	var videos = document.getElementsByTagName("video");
+	var videos = document.getElementsByTagName("video"); // get all video
+
+	// Store the video element in an array
 	const videoItem = [];
 	for (let i = 0; i < videos.length; i++) {
 		videoItem.push(videos.item(i));
 	}
 
+	// Loop through the video element array
 	videoItem.forEach(function (video) {
+		// add event listener to the window with key event that binds to the video
 		window.addEventListener("keydown", function (event) {
 			// use https://keycode.info/ to find the keycode
 			// check if key press is ctrl + ]
---
+++ readme.md
@@ -0,0 +1,4 @@
+# About
+Ever want to seek forward a video for only a few seconds? Well you can actually do it easily. This script does that. 
+
+You can use `ctrl + \` to seek forward and `ctrl + ]` to seek backward for 3 seconds. You can easily change the key and time easily in the script itself. I have leave a few comments in the script to explain how it works.
