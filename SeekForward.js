e1d91c5e9104becfd16ced683ce4d3b3264fb1b2
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-10 10:58:52 +0700

    userscript info update

--- SeekForward.js
+++ SeekForward.js
@@ -4,21 +4,22 @@
 // @version      1.0
 // @description  Use ctrl + \ and ctrl + ] to seek video forward and backward 3 seconds. To seek forward use ctrl + \. To seek backward use ctrl + ]. You can modify the key and the seek time to your desire easily in the script.
 // @author       Dadangdut33
+// @license Unlicense
 // @include *
 // @run-at document-load
 // ==/UserScript==
-
+ 
 (function () {
 	"use strict";
-
+ 
 	var videos = document.getElementsByTagName("video"); // get all video
-
+ 
 	// Store the video element in an array
 	const videoItem = [];
 	for (let i = 0; i < videos.length; i++) {
 		videoItem.push(videos.item(i));
 	}
-
+ 
 	// Loop through the video element array
 	videoItem.forEach(function (video) {
 		// add event listener to the window with key event that binds to the video
